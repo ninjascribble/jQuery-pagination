994ba46fbb7413b4b1cd72d5da1ca500327306df
Scott Grogan <scott@ninjascript.com>
2012-08-20 22:47:29 -0700

    Some working next/prev pagination

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,38 @@
 ;(function($) {
 
+	var paged_items_container = $('#items').css({ opacity: 0, height: 760, overflow: 'hidden' })
+	  , paged_items_controls = $('#controls')
+	  , paged_items = $('#items ul').pagination({
+			pp: 12,
+			before_change: beforePageChange,
+			after_change: afterPageChange
+		});
+
+	paged_items_controls
+		.on('click', 'button.prev', function() { navigateTo('previous'); })
+		.on('click', 'button.next', function() { navigateTo('next'); });
+
+	paged_items
+		.on('page-out-of-bounds', onPageOutOfBounds);
+
+	function navigateTo(n) {
+		paged_items.pagination(n);
+	}
+
+	function beforePageChange(callback, context) {
+		paged_items_container.animate({ opacity: 0 }, function() {
+			callback.call(context);
+		});
+	}
+
+	function afterPageChange(callback, context) {
+		paged_items_container.animate({ opacity: 1 }, function() {
+			callback.call(context);
+		});
+	}
+
+	function onPageOutOfBounds(e) {
+		console.log(e.type, e.requested_page, e);
+	}
+
 }(jQuery));
---
+++ public/javascripts/jquery.pagination.js
@@ -0,0 +1,85 @@
+;(function($) {
+
+	var defaults = {
+		pp: 8,
+		before_change: noop,
+		after_change: noop
+	};
+
+	$.fn.extend({ pagination: function(opts) {
+		if ($.isPlainObject(opts)) {
+			init.call(this, opts);	
+		}
+		else {
+			switch(opts) {
+				case 'next':
+					showPage.call(this, this.page + 1);
+					break;
+				case 'previous':
+					showPage.call(this, this.page - 1);
+					break;
+				case 'status':
+					return getStatus.call(this);
+				default:
+					break;
+			}
+		}
+		return this;
+	}});
+
+	function init(opts) {
+		this.opts = $.extend({}, defaults, opts);
+		this.items = this.children('li');
+		this.num_pages = Math.ceil(this.items.length / this.opts.pp);
+		showPage.call(this, 1);
+	}
+
+	function showPage(n) {
+
+		if (n <= 0 || n > this.num_pages) {
+			triggerEvent.call(this, 'page-out-of-bounds', { requested_page: n });
+			return false;
+		}
+
+		this.opts.before_change(function() {
+
+			var page = this.page = (n || 1)
+			  , range = [(this.opts.pp * this.page) - this.opts.pp, this.opts.pp * this.page];
+
+			while (this.items.length <= range[0]) {
+				page = this.page = page - 1;
+				range = [(this.opts.pp * this.page) - this.opts.pp, this.opts.pp * this.page];
+			}
+
+			this.items.hide().each(function(i, el) {
+				if (i >= range[0] && i < range[1]) {
+					$(el).show();
+				}
+			});
+			
+			this.opts.after_change(function() {
+				triggerEvent.call(this, 'page-changed');
+			}, this);
+
+		}, this);
+
+	}
+
+	function getStatus() {
+		return {
+			page: this.page,
+			num_pages: this.num_pages
+		}
+	}
+
+	function triggerEvent(type, more) {
+		this.trigger($.extend({ type: type }, getStatus.call(this), more));
+	}
+
+	function noop(callback, context) {
+		if($.isFunction(callback)) {
+			callback.call(context);
+		}
+	}
+
+}(jQuery));
